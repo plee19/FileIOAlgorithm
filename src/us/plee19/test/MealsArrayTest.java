25eb314c23308516cbefaec78e20f07aabaa4cc8
plee19 <42356593+plee19@users.noreply.github.com>
2018-09-10 07:46:21 -0500

    Attempts at FileIO setup with MealsArray to emulate printout

--- src/us/plee19/test/MealsArrayTest.java
+++ src/us/plee19/test/MealsArrayTest.java
@@ -6,16 +6,25 @@ import org.junit.Before;
 import org.junit.Test;
 import us.plee19.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class MealsArrayTest extends TestCase {
 
-    MealsArray meals = new MealsArray();
+    MealsArray meals;
+    FileInput inputTest;
+    private final ByteArrayOutputStream outStuff = new ByteArrayOutputStream();
+    private final PrintStream original = System.out;
 
     @Before
     public void setUp() throws Exception {
+        meals = new MealsArray();
+        System.setOut(new PrintStream(outStuff));
     }
 
     @After
     public void tearDown() throws Exception {
+        System.setOut(original);
     }
 
     @Test
@@ -29,18 +38,26 @@ public class MealsArrayTest extends TestCase {
         assertNotNull(meals.getMeals());
     }
 
+    // Issues with proper output in void methods
     @Test
     public void testPrintAllMeals() {
+        meals.addElementWithStrings("Dinner", "pizza", "300");
         meals.printAllMeals();
+        assertEquals("Meals{mealType=Dinner, item='pizza', calories=300}\r\n", outStuff.toString());
     }
 
     @Test
     public void testPrintMealsByType() {
+        meals.addElementWithStrings("Dinner", "pizza", "300");
         meals.printMealsByType(MealType.DINNER);
+        assertEquals("Meals{mealType=Dinner, item='pizza', calories=300}\r\n", outStuff.toString());
     }
 
+    //
     @Test
     public void testPrintByNameSearch() {
+        meals.addElementWithStrings("Dinner", "pizza", "300");
         meals.printByNameSearch("pizza");
+        assertEquals("Meals{mealType=DINNER, item='pizza', calories=300}\r\n",  outStuff.toString());
     }
 }
